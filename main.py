e1f85b79bd2b76b89134949d5b87c7fa1d9ee90f
Dev Saran Sujan <97866263+touchhowling@users.noreply.github.com>
2023-08-26 09:58:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class TestInput(BaseModel):
     P_NAME: Optional[str]
     P_STATUS: int
